5e6a7d5684564fea958440b1b894efdc5c8ea6f2
yanishevska <yanishevska1989@gmail.com>
2023-03-03 10:18:37 +0100

    add js

---
+++ index.js
@@ -0,0 +1,33 @@
+const refs={
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modalBackdrop: document.querySelector('[data-modal]'),
+    }
+    
+    refs.openModalBtn.addEventListener('click', openModal);
+    refs.closeModalBtn.addEventListener('click', closeModal);
+    refs.modalBackdrop.addEventListener('click', closeModalOverlay);
+    
+    function openModal() {
+      window.addEventListener('keydown', closeModalESC);
+      document.body.classList.toggle('pop-up-open');
+      refs.modalBackdrop.classList.remove('is-close');
+    }
+    
+    function closeModalESC(event) {
+      if (event.code === 'Escape') {
+        closeModal();
+      }
+    }
+    
+    function closeModalOverlay(event) {
+      if (event.target === event.currentTarget) {
+        closeModal();
+      }
+    }
+    
+    function closeModal() {
+      window.removeEventListener('keydown', closeModalESC);
+      document.body.classList.toggle('pop-up-open');
+      refs.modalBackdrop.classList.add('is-close');
+    }
